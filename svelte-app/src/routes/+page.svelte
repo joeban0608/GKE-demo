808c5c1d613a01fa6eef85fdafadd4db5b976291
joeban <joeanddaneil5@gmail.com>
2025-02-26 12:14:59 +0800

    feat: add svelte app

---
+++ svelte-app/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1 class="text-4xl">Welcome to SvelteKit</h1>
+<p>
+	Visit
+	<a
+		class="transform text-blue-500 transition-all duration-300 hover:scale-110 hover:text-blue-700"
+		href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a
+	>
+	to read the documentation
+</p>
